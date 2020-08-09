f7317e437bac4979203edb1c3f352a43a034e76b
Jayme Frantz <hello@jaymefrantz.com>
2020-08-09 19:33:38 -0400

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <p><Flip v-bind:value="counter"/></p>
+    <button @click="increase">Increase</button>
+  </div>
+</template>
+
+<script>
+import Flip from "~/components/Flip";
+
+export default {
+  name: "App",
+  components: {
+    Flip
+  },
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    increase() {
+      this.counter++;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
